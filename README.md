7f5683f8c9fb1f7c9cc79cd85333afed5a447bea
David Maas <contact@pathogenstudios.com>
2020-08-21 02:29:30 -0500

    Renamed test parameters, added descriptions, and added same-value different-reference tests.

--- README.md
+++ README.md
@@ -4,9 +4,22 @@ This benchmark compares the performance of the overloaded `==` operator and the 
 
 Here's the current benchmark results from my machine:
 
-|                  Method |       Mean |     Error |    StdDev |
-|------------------------ |-----------:|----------:|----------:|
-|        EqualsMethodSame | 27.0407 ns | 0.3633 ns | 0.3398 ns |
-|      EqualsOperatorSame |  0.3419 ns | 0.0384 ns | 0.0395 ns |
-|   EqualsMethodDifferent |  9.9067 ns | 0.1500 ns | 0.1403 ns |
-| EqualsOperatorDifferent | 10.3294 ns | 0.0801 ns | 0.0710 ns |
+|         Method |       Mean |     Error |    StdDev | Ratio | RatioSD |
+|--------------- |-----------:|----------:|----------:|------:|--------:|
+|  `x.Equals(x)` | 27.0569 ns | 0.2132 ns | 0.1890 ns | 84.25 |    8.87 |
+|     '`x == x`' |  0.3225 ns | 0.0378 ns | 0.0354 ns |  1.00 |    0.00 |
+| `x.Equals(x2)` | 26.4226 ns | 0.1043 ns | 0.0925 ns | 82.30 |    8.88 |
+|    '`x == x2`' | 26.4400 ns | 0.3351 ns | 0.2616 ns | 81.22 |    9.44 |
+|  `x.Equals(y)` | 10.2818 ns | 0.1136 ns | 0.1063 ns | 32.22 |    3.36 |
+|     '`x == y`' |  9.7670 ns | 0.0641 ns | 0.0568 ns | 30.42 |    3.23 |
+
+(`x2` is a record with the same values as `x`, but a different reference.)
+
+**Legend**:
+
+* Mean    : Arithmetic mean of all measurements
+* Error   : Half of 99.9% confidence interval
+* StdDev  : Standard deviation of all measurements
+* Ratio   : Mean of the ratio distribution ([Current]/[Baseline])
+* RatioSD : Standard deviation of the ratio distribution ([Current]/[Baseline])
+* 1 ns    : 1 Nanosecond (0.000000001 sec)
--- RecordEqualityPerformance/Program.cs
+++ RecordEqualityPerformance/Program.cs
@@ -17,22 +17,40 @@ public record FatRecord
 
 public class Benchmark
 {
-    public readonly FatRecord A = new();
-    public readonly FatRecord AlsoA;
-    public readonly FatRecord B = new();
+    public readonly FatRecord X = new();
+    /// <summary>Same reference and value as <see cref="X"/></summary>
+    /// <remarks>This doesn't really need to be separate, it's really only here to suppress the warning you get when comparing a reference to its self.</remarks>
+    public readonly FatRecord AlsoX;
+    public readonly FatRecord Y = new();
+    /// <summary>Same value as <see cref="X"/>, but different reference</summary>
+    public readonly FatRecord X2;
 
     public Benchmark()
-        => AlsoA = A;
+    {
+        AlsoX = X;
 
-    [Benchmark]
-    public bool EqualsMethodSame() => A.Equals(AlsoA);
+        // Clone X withoutout changing anything
+        X2 = X with { G = X.G };
 
-    [Benchmark]
-    public bool EqualsOperatorSame() => A == AlsoA;
+        if (ReferenceEquals(X, X2))
+        { throw new InvalidOperationException(); }
+    }
 
-    [Benchmark]
-    public bool EqualsMethodDifferent() => A.Equals(B);
+    [Benchmark(Description = "`x.Equals(x)`")]
+    public bool EqualsMethodSame() => X.Equals(AlsoX);
 
-    [Benchmark]
-    public bool EqualsOperatorDifferent() => A == B;
+    [Benchmark(Description = "`x == x`", Baseline = true)]
+    public bool EqualsOperatorSame() => X == AlsoX;
+
+    [Benchmark(Description = "`x.Equals(x2)`")]
+    public bool EqualsMethodEquivalent() => X.Equals(X2);
+
+    [Benchmark(Description = "`x == x2`")]
+    public bool EqualsOperatorEquivalent() => X == X2;
+
+    [Benchmark(Description = "`x.Equals(y)`")]
+    public bool EqualsMethodDifferent() => X.Equals(Y);
+
+    [Benchmark(Description = "`x == y`")]
+    public bool EqualsOperatorDifferent() => X == Y;
 }
